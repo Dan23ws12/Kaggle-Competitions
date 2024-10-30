fe10bf89a984e24fa738734d9382025f9a7712de
Dan23ws12 <danieloyeleye247@gmail.com>
2024-10-30 00:27:41 -0400

    scaled the test and training data

--- Loan Prediction/Random Forest.R
+++ Loan Prediction/Random Forest.R
@@ -20,9 +20,11 @@ loans <- factorize(loans)
 submission <- factorize(submission)
 #a subject's age and their credit history length is highly correlated
 #thus I will remove one (prob a person's age)
-loans <- loans %>% select(!c(person_age))
+#also amount loaned and the loan as percent of income are highly correlated
+#will remove loan as percent of income
+loans <- loans %>% select(!c(person_age, loan_percent_income))
 sub_id <- submission$id
-submission <- submission %>% select(!c(person_age, id))
+submission <- submission %>% select(!c(person_age, id, loan_percent_income))
 
 #turning loan_status to a factor
 loans$loan_status <- factor(loans$loan_status, levels = c(1, 0),labels = c("accepted", "rejected"))
@@ -31,15 +33,37 @@ loans$loan_status <- factor(loans$loan_status, levels = c(1, 0),labels = c("acce
 #splitting data into test and training data
 train_df <- loans %>% sample_frac(size = 0.7)
 test_df <- loans %>% anti_join(train_df)
-x_train <- train_df %>% select(!c(loan_status))
-y_train <- train_df$loan_status
-x_test <- test_df %>% select(!c(loan_status))
-y_test <- test_df$loan_status
-
 
 #scaling the test and training data
-numeric_vars <- c("person_income","person_emp_length","loan_amnt",
-                  "loan_int_rate","loan_percent_income","cb_person_cred_hist_length")
+get_center_scale <- function(train_data){
+  #returns the mean and standard deviation of the training data 
+  #should only be called FOR TRAINING DATA (to scale test data)
+  numeric_vars <- c("person_income","person_emp_length","loan_amnt",
+                    "loan_int_rate","cb_person_cred_hist_length")
+  scale_ <- apply(train_df[, numeric_vars], 2, sd)
+  center <- apply(train_df[, numeric_vars], 2, mean)
+  return(data.frame(center = center, scale_ = scale_))
+}
+
+scale_and_join <- function(df, center_, deviation){
+  #scales the test and training data with the given center and deviation/scale
+  numeric_vars <- c("person_income","person_emp_length","loan_amnt",
+                    "loan_int_rate","cb_person_cred_hist_length")
+  categorical_vars <- c("person_home_ownership","loan_intent","loan_grade",
+                        "cb_person_default_on_file", "loan_status")
+  df_scaled <- data.frame(scale(df[, numeric_vars], 
+                                   center = center_, scale = deviation))
+  df_scaled <- cbind.data.frame(df_scaled, df[, categorical_vars])
+  return(df_scaled)
+}
+train_param <- get_center_scale(train_data = train_df)
+train_scaled <- scale_and_join(train_df, train_param$center, train_param$scale_)
+test_scaled <- scale_and_join(test_df, train_param$center, train_param$scale_)
+
+x_train <- train_scaled %>% select(!c(loan_status))
+y_train <- train_scaled$loan_status
+x_test <- test_scaled %>% select(!c(loan_status))
+y_test <- test_scaled$loan_status
 
 #training the model
 rand_forest_md <- randomForest(x=x_train, y=y_train, importance=TRUE)
